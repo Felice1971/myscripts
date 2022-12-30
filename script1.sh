b5ca7a19eca6f27f4b6d1512206a326e0fd5e430
Felice1971 <Felice.Tamofo@hotmail.it>
2022-12-30 03:26:12 +0100

    First scripts

---
+++ script1.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Hello World"
+
+
+myname="Felix"
+myage="52"
+
+
+echo "my name is $myname"  
+
+echo "I'm $myage old"       
+
+echo "Nice to meet you!"     
+
+
+
+runing=$(uptime)
+
+memory=$(free -m)
+
+echo " This server has been up and runing from : $runing" 
+
+echo " The memory size of this computer is as follow : $memory"
+
+echo " The current username is: $USER"
+
+expr 10 + 6
+
+expr 10 / 2
+
+mynum1="300"
+
+echo "$mynum1 / 3"
+
+
+mynum2="2"
+
+echo "$mynum1 + $mynum2"
+
+
+echo "$mynum1 - 125"
+
+
---
+++ script_if.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+mynum=150
+
+if [ $mynum -eq 200 ]
+then
+	echo "The condition is true!"
+else 
+
+	echo " The condition is not true!"
+fi
+
+
+
+if [ -f ~/script1.sh ]
+then
+
+echo "The file exists!"
+else 
+echo "The file does not exists!"
+
+fi
+
+
